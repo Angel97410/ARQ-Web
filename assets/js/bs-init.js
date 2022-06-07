1d81fed0ab8bf890214203e5b449d7ab68180309
Angélique Robert <angel97410.ar@gmail.com>
2022-06-07 21:53:19 +0200

    ajout nodemailer

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,4 +1,3 @@
-
 if (window.innerWidth < 768) {
 	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
 		elem.classList.remove('animated');
@@ -7,6 +6,31 @@ if (window.innerWidth < 768) {
 	});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 	AOS.init();
 }, false);
+
+
+// formulaire de Contact
+
+const nodemailer = require("nodemailer");
+nodemailer.createTransport({
+  host: "smtp-mail.outlook.com",
+  port: 587,
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: "robert.angelique@outlook.com",
+    pass: "Angel97410",
+  },
+});
+
+let transporter = nodemailer.createTransport(transport[ defaults])
+let poolConfig = "smtps://robert.angelique@outlook.com:Angel97410@smtp-mail.outlook.com/?pool=true";
+
+var message = {
+  from: document.getElementsByClassName('email'),
+  to: "robert.angelique@hotmail.fr",
+  subject: "Contact ARQ-Web",
+  text: document.getElementsByClassName('message'),
+  html: "<p>"+text+"</p>"
+};
